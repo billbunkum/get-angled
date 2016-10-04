d26e8111ab98965618217cc58bad087872870f23
billbunkum <william.richard.hurst@gmail.com>
2016-10-04 13:52:38 -0400

    finish

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ angular.module('app', [])
 function mainController($scope) {
     $scope.userRows = 12;
     $scope.userCols = 12;
+    $scope.selectedValue = null;
 
     $scope.getRows = function() {
         return R.range(1, $scope.userRows+1);
@@ -18,6 +19,17 @@ function mainController($scope) {
 
     $scope.showTable = function() {
         return $scope.userRows > 0 && $scope.userCols > 0;
+    }
+
+    $scope.setSelectedValue = function(row, col) {
+        $scope.selectedValue = {
+            row: row,
+            col: col
+        }
+    }
+
+    $scope.clearSelectedValue = function() {
+        $scope.selectedValue = null;
     }
 
 }
