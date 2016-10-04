a2b715a7e21ac87c2e7401f9a0db094e844a28f5
billbunkum <william.richard.hurst@gmail.com>
2016-10-04 11:52:47 -0400

    angled table app basically done

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('app', [])
+    .controller('MainCtrl', mainController);
+
+function mainController($scope) {
+    $scope.userRows = 12;
+    $scope.userCols = 12;
+
+    $scope.getRows = function() {
+        return R.range(1, $scope.userRows+1);
+    }
+
+    $scope.getCols = function() {
+        return R.range(1, $scope.userCols+1);
+    }
+    $scope.getTableValue = function(row, col) {
+        return row * col;
+    }
+
+    $scope.showTable = function() {
+        return $scope.userRows > 0 && $scope.userCols > 0;
+    }
+
+}
